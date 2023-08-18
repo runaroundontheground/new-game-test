bf7390ce5760f02993133541c1b2c05bcc5b59e9
runaroundontheground <118295064+runaroundontheground@users.noreply.github.com>
2023-08-18 16:48:34 +0000

    message

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import pygame as pg
 
 
 pg.init()
-print("test")
+
+display = pg.display.set_mode((100, 100))
+rect = pg.Rect(0, 0, 20, 20)
+
+display.fill((255, 255, 255))
+pg.display.flip()
+input()
 
 pg.quit()
