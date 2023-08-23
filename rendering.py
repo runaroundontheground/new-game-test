572c32eb77b6386c750dd1cac137d117068713a4
runaroundontheground <118295064+runaroundontheground@users.noreply.github.com>
2023-08-23 15:46:37 +0000

    still working on rendering

--- rendering.py
+++ rendering.py
@@ -2,14 +2,16 @@ import pygame
 
 screenWidth, screenHeight = 600, 400
 screen = pygame.display.set_mode((screenWidth, screenHeight))
-blockSize = (100, 100)
+blockSize = (30, 30)
 gray = pygame.surface.Surface((blockSize[0], blockSize[1]))
 gray.fill((50,50,50))
 lightGray = pygame.surface.Surface((blockSize[0], blockSize[1]))
 lightGray.fill((100,100,100))
 white = pygame.surface.Surface((blockSize[0], blockSize[1]))
 white.fill((255, 255, 255))
-blockImages = [gray, lightGray, white]
+air = pygame.surface.Surface((blockSize[0], blockSize[1]))
+air.fill((0, 255, 0, 255))
+blockImages = [air, gray, lightGray, white]
 import random as r
 chunk = {
     (0, 0, 0): 0
@@ -19,26 +21,28 @@ def createChunk():
         for y in range(3):
             for z in range(10):
                 
-                chunk[(x,y,z)] = r.randint(0, 1)
+                chunk[(x,y,z)] = r.randint(1, 2)
+    chunk[(r.randint(0, 10), 2, r.randint(0, 10))] = 0
+    chunk[(r.randint(0, 10), 2, r.randint(0, 10))] = 0
+
 
 print(chunk)
-layer = 1
+layer = 3
 
 def render():
-    screen.fill((255, 255, 255))
-    createChunk()
+    global layer
+    screen.fill((0, 0, 0))
+    
     blocks = []
     for x in range(10):
         for z in range(10):
             block = chunk[(x, layer, z)]
-            if block == 1:
-                imageData = (lightGray, (x*blockSize[0], z*blockSize[1]))
-            elif block == 0:
-                imageData = (gray, (x*blockSize[0], z*blockSize[1]))
+            image = blockImages[block]
+            image = pygame.transform.scale_by(image, layer+1)
+            imageData = (image, (x*blockSize[0], z*blockSize[1]))
             blocks.append(imageData)
     
     screen.blits(blocks)
-    layer += 1
-    if layer > 3: layer = 0
+    
 
     pygame.display.flip()
