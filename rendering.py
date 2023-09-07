4b4c9517991ec10c579c0538141d951aa46ea671
runaroundontheground <118295064+runaroundontheground@users.noreply.github.com>
2023-09-07 15:57:23 +0000

    started setting up chunk based system

--- rendering.py
+++ rendering.py
@@ -1,67 +1,84 @@
 import pygame
 pygame.font.init()
+
 font = pygame.font.Font(size = 24)
+
 screenWidth, screenHeight = 600, 400
 screen = pygame.display.set_mode((screenWidth, screenHeight))
+
 blockSize = (30, 30)
 heightLimit = 6
-gray = pygame.surface.Surface((blockSize[0], blockSize[1])); gray.fill((50,50,50))
-lightGray = pygame.surface.Surface((blockSize[0], blockSize[1])); lightGray.fill((100,100,100))
-white = pygame.surface.Surface((blockSize[0], blockSize[1])); white.fill((255,255,255))
-air = 0
-green = pygame.surface.Surface((blockSize[0], blockSize[1])); green.fill((0, 255, 0))
-red = pygame.surface.Surface((blockSize[0], blockSize[1])); red.fill((255, 0, 0))
-brown = pygame.surface.Surface((blockSize[0], blockSize[1])); brown.fill((200, 75, 0))
-numbers = []
+chunkSize = (10, heightLimit, 10)
 
+colors = [
+    0, # air
+    pygame.surface.Surface((blockSize[0], blockSize[1])), # gray
+    pygame.surface.Surface((blockSize[0], blockSize[1])), # lightGray
+    pygame.surface.Surface((blockSize[0], blockSize[1])), # white
+    pygame.surface.Surface((blockSize[0], blockSize[1])), # green
+    pygame.surface.Surface((blockSize[0], blockSize[1])), # red
+    pygame.surface.Surface((blockSize[0], blockSize[1])) # brown
+]
+def fillColor(colorNum, colorValue):
+    colors[colorNum].fill(colorValue)
+fillColor(1, (50, 50, 50))
+fillColor(2, (100, 100, 100))
+fillColor(3, (255, 255, 255))
+fillColor(4, (0, 255, 0))
+fillColor(5, (255, 0, 0))
+fillColor(6, (200, 75, 0))
+numbers = []
+print(colors[0])
 def makeNumbers():
     for num in range(10):
         print(num)
         number = font.render(str(num), 0, (200, 200, 200))
         numbers.append(number)
 makeNumbers()
-blockImages = [air, gray, lightGray, white, green, red, brown]
+blockImages = [colors[0], colors[1], colors[2], colors[3], colors[4], colors[5], colors[6]]
 import random as r
-chunk = {
-    (0, 0, 0): 0
+chunks = {
+    (0, 0): (0, 1, 0)
 }
-def createChunk():
+
+def createChunk(chunkCoords = (0, 0)):
     for x in range(10):
         for y in range(heightLimit):
             for z in range(10):
-                coord = (x,y,z)
-                chunk[coord] = 0
-
-                if y == 0: chunk[coord] = 1
-                if y == 1:
-                    if x > 2:
-                        chunk[coord] = 2
-                if y == 2:
-                    if x > 4:
-                        chunk[coord] = 3
-                if y == 3:
-                    if x > 5:
-                        chunk[coord] = 4
-                if y == 4:
-                    if x > 6:
-                        chunk[coord] = 5
-                if y == 5:
-                    if x > 8:
-                        chunk[coord] = 6
-
+                chunkCoord = chunkCoords
+                blockCoord = (x,y,z)
+                block = 0
                 
 
-createChunk()
+                if y == 0: block = 1
+                if y == 1: block = 2
+                if y == 2: block = 3
+                if y == 3: block = 4
+                if y == 4: block = 5
+                if y == 5: block = 6
+                if r.randint(0, 3) == 0:
+                    block =
+                
+                chunks[chunkCoord][blockCoord] = block
 
+createChunk((0, 0))
+createChunk((0, 1))
+createChunk((1, 0))
+createChunk((1, 1))
 
 layer = 0
 def render(keysPressed):
     global layer
     screen.fill((0, 0, 255))
     
-    if keysPressed[pygame.K_PERIOD] and layer < heightLimit: layer += 1
+    if keysPressed[pygame.K_PERIOD] and layer < heightLimit - 1: layer += 1
     if keysPressed[pygame.K_COMMA] and layer > 0: layer -= 1
     blocks = []
+
+    # get the chunks to be used for rendering
+
+
+
 
     for x in range(10):
         for y in range(heightLimit):
@@ -73,14 +90,15 @@ def render(keysPressed):
                     else:
                         factor = 1
                         if y > layer:
-                            factor += (y - layer)/5
+                            factor += (y - layer) / 10
                         if y < layer:
-                            factor -= (layer - y)/5
+                            factor -= (layer - y) / 10
+                        
                         
                         image = blockImages[block].copy()
                         image = pygame.transform.scale_by(image, abs(factor))
                         image.blit(numbers[y], (0, 0))
-                        imageData = (image, (x*blockSize[0]*factor, z*blockSize[1]*factor))
+                        imageData = (image, (x*blockSize[0], z*blockSize[1]))
 
                     blocks.append(imageData)
     
