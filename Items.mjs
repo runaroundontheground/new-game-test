9a61dc464ba2ddbe7da233da9f0a9e4a251dc03d
runaroundontheground <118295064+runaroundontheground@users.noreply.github.com>
2024-02-14 19:03:35 +0000

    porting items.mjs now

--- Items.mjs
+++ Items.mjs
@@ -10,119 +10,134 @@ import { mouse } from "./Controls.mjs";
 import { ItemEntity } from "./Entities.mjs";
 
 
-# basic item, default values to make it exist in player inventory properly
-class Item():
-    def __init__(self, name = "air"):
-        self.name = name
-        self.slotId = 0
-        self.tooltip = "tooltip unavailable"
+class Item {
+    constructor (name) {
+
+
+        this.name = name || "air";
+        this.slotId = 0;
+        this.tooltip = "tooltip unavailable";
+        this.stackable = false;
         
     
-    # drop/throw this item out as an entity
-    def drop(self, x, y, z, xv, yv, zv, player = None, count = 1):
+        this.drop = function (x, y, z, xv, yv, zv, player = undefined, count = 1) {
 
-        droppedItem = ItemEntity(self, count, x, y, z, xv, yv, zv)
+        let droppedItem = new ItemEntity(x, y, z, xv, yv, zv, count, this);
 
-        entities.append(droppedItem)
+        entities.push(droppedItem);
 
-        if player != None:
-            if player.hotbar[self.slotId]["count"] == 1:
-                player.hotbar[self.slotId]["contents"] = "empty"
-            elif player.hotbar[self.slotId]["count"] >= 2:
-                player.hotbar[self.slotId]["count"] -= 1
+        if (player !== undefined) {
+            if (player.hotbar[this.slotId]["count"] == 1) {
+                player.hotbar[this.slotId]["contents"] = "empty";
+            } else {
+                if (player.hotbar[this.slotId].count >= 2) {
+                    player.hotbar[this.slotId]["count"] -= 1
+                };
+            };  
+        };
+    };
 
-# an item that can be placed, like wood or something
-class PlaceableItem(Item):
-    def __init__(self, name, tooltip = "", stackable = True):
-        super().__init__(name)
-        self.tooltip = self.name
-        if tooltip != "":
-            self.tooltip = tooltip
-        self.slotId = 0
-        self.itemType = "PlaceableItem"
-        self.stackable = stackable
-        self.placedBlock = {
-            "type": self.name,
-            "render": False,
+    };
+};
+
+
+class PlaceableItem extends Item {
+    constructor (stackable) {
+        super(); 
+        
+        this.stackable = stackable || true;
+        this.itemType = "PlaceableItem";
+
+        this.placedBlock = {
+            "type": this.name,
+            "render": false,
             "alphaValue": 255,
             "hardness": 0,
             "effectiveTool": "none"
-        }
-        self.placedBlock["hardness"] = dictOfBlockBreakingStuff[self.name]["hardness"]
-        self.placedBlock["effectiveTool"] = dictOfBlockBreakingStuff[self.name]["effectiveTool"]
+        };
 
-    def placeItem(self, player):
+        this.placedBlock.hardness = dictOfBlockBreakingStuff[this.name].hardness;
+        this.placedBlock.effectiveTool = dictOfBlockBreakingStuff[this.name].effectiveTool;
 
-        if player.canReachSelectedBlock:
-            blockType = mouse.hoveredBlock["block"]["type"]
-            if blockType == "air" or blockType == "water":
-                count = player.hotbar[self.slotId]["count"]
-                if count > 1:
-                    player.hotbar[self.slotId]["count"] -= 1
-                    
-                elif count == 1:
-                    player.hotbar[self.slotId]["count"] = 0
-                    player.hotbar[self.slotId]["contents"] = "empty"
-                    
-                elif count <= 0:
-                    print("what the heck, how did you do that??\nthe item count is invalid or something")
+    this.placeItem = function (player) {
 
-                chunkCoord = mouse.hoveredBlock["chunkCoord"]
-                blockCoord = mouse.hoveredBlock["blockCoord"]
+        if (player.canReachSelectedBlock) {
+            let blockType = mouse.hoveredBlock.block.type;
+            if (blockType == "air" || blockType == "water") {
+                let count = player.hotbar[this.slotId].count;
+
+                switch (count) {
+                    case count > 1:
+                        player.hotbar[this.slotId].count -= 1; break;
+                    case count == 1:
+                        player.hotbar[this.slotId].count = 0;
+                        player.hotbar[this.slotId].contents = "empty";
+                        break;
+                    case count <= 0:
+                        consooeLog("how'd you do that??");
+                }
                 
 
-                chunks[chunkCoord]["data"][blockCoord] = self.placedBlock.copy()
+                let chunkCoord = mouse.hoveredBlock.chunkCoord;
+                let blockCoord = mouse.hoveredBlock.blockCoord;
+                
+
+                chunks[chunkCoord].data[blockCoord] = this.placedBlock;
 
                 smallScaleBlockUpdates(chunkCoord, blockCoord)
+            };
+        };
 
+    };
 
-
-    def RMBAction(self, player):
+    def RMBAction(this, player):
         pass
         
 
-    def RMBPressedAction(self, player):
-        self.placeItem(player)
+    def RMBPressedAction(this, player):
+        this.placeItem(player)
+    };
+};
 
 class ToolItem(Item):
-    def __init__(self, name, toolData = {"attack": 1, "knockback": 1,
+    def __init__(this, name, toolData = {"attack": 1, "knockback": 1,
                                          "breakingPower": 1, "breakingSpeed": 1,
                                          "breakingType": "none"},
-                                         tooltip = "", stackable = False):
+                                         tooltip = "", stackable = false):
         super().__init__(name)
-        self.slotId = 0
-        self.itemType = "ToolItem"
-        self.tooltip = self.name
+        this.slotId = 0
+        this.itemType = "ToolItem"
+        this.tooltip = this.name
         if tooltip != "":
-            self.tooltip = tooltip
-        self.stackable = stackable
+            this.tooltip = tooltip
+        this.stackable = stackable
         
-        self.attack = toolData["attack"]
-        self.knockback = toolData["knockback"]
-        self.breakingPower = toolData["breakingPower"]
-        self.breakingSpeed = toolData["breakingSpeed"]
-        self.breakingType = toolData["breakingType"]
+        this.attack = toolData["attack"]
+        this.knockback = toolData["knockback"]
+        this.breakingPower = toolData["breakingPower"]
+        this.breakingSpeed = toolData["breakingSpeed"]
+        this.breakingType = toolData["breakingType"]
 
-        self.durability = 100 # durability will exist when the game actually functions
+        this.durability = 100 # durability will exist when the game actually functions
     
-    def RMBPressedAction(self, player):
+    def RMBPressedAction(this, player):
         pass
 
-    def RMBAction(self, player):
+    def RMBAction(this, player):
         pass
 
 class IntermediateItem(Item):
-    def __init__(self, name, stackable = True):
+    def __init__(this, name, stackable = true):
         super().__init__(name)
-        self.stackable = stackable
-        self.tooltip = self.name
-        self.slotId = 0
+        this.stackable = stackable
+        this.tooltip = this.name
+        this.slotId = 0
 
 # adding items to the game
-def addItem(name = "air", itemType = "none", toolData = {}, stackable = False):
+def addItem(name = "air", itemType = "none", toolData = {}, stackable = false):
 
     if itemType == "placeable":
-        item = PlaceableItem(name, stackable = True)
+        item = PlaceableItem(name, stackable = true)
     
     if itemType == "tool":
         item = ToolItem(name, toolData)
@@ -133,9 +148,9 @@ def addItem(name = "air", itemType = "none", toolData = {}, stackable = False):
 
 def makeItemsExist():
     for itemName in listOfBlockNames:
-        addItem(itemName, "placeable", stackable = True)
+        addItem(itemName, "placeable", stackable = true)
     for itemName in listOfIntermediateItems:
-        addItem(itemName, "intermediate", stackable = True)
+        addItem(itemName, "intermediate", stackable = true)
     addItem("stone pickaxe", "tool",
             {"attack": 3, "knockback": 1, "breakingPower": 3,
             "breakingSpeed": 20, "breakingType": "pickaxe"})
